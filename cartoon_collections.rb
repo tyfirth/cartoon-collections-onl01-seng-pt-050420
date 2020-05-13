b4be9ed1a9bd8a2a33ba5c2dc28a7e0f7949ddef
Tyler Firth <tyfirth@gmail.com>
2020-05-13 03:59:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(calls)
   calls.collect { |call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(cheers)
+  if cheers.any?
 end
 
 def find_the_cheese# code an argument here
