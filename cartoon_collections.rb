88a850a3748b540db0a28ab73e4d4f689932f1b8
Tyler Firth <tyfirth@gmail.com>
2020-05-13 04:12:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(cheers)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
